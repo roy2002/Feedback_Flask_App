d966e373d0a5f49891de931b3ed2fecde7803990
roy2002 <soumikroychoudhury02@gmail.com>
2023-02-19 09:28:15 +0530

    2nd Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        print(customer, dealer, rating, comments)
+        return render_template('success.html')
 
 
 if __name__ == '__main__':
